06c9fb325c1c48cba0423e21961a41f96c71a9c9
kondurusashank <msashank@outlook.com>
2022-08-05 16:27:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def genjira_url=""
 def estjira_url= ""
 def amap
@@ -19,4 +20,115 @@ pipeline {
                 }
             }
         }
-        stage('create_tast_estjira') {}
+        stage('create_tast_estjira') {
+            steps{
+                script{
+                    def file = readFile "genjira_tasks.txt"
+                    def lines = file.readLines()
+                    
+                    file.split('\n').each {String line ->
+                        wrap([$class:'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${password}", var: 'PSWD']]]) {
+                            def resp = sh(script: ''' curl -k -u ${username}: $(password) url ''' + line+ ''' | python -mjoson tool > jira_details.json ''', returnStdout:true)
+                            echo resp
+                        }
+                        
+                        def props = readJSON file: 'jira_details.json'
+                        summary_value = "${props.fields.summary}"
+                        issuetype_name="${}"
+                        reporter_name="${}"
+                        description="${}"
+                        fxver_arch="${}"
+                        echo "Fix Version:${fxver_arch}"
+                        fxver_name="${}"
+                        fxver_reldate="${}"
+                        fxver_rel="${}"
+                        priority_name="${}"
+                        labels="${}"
+                        components="${}"
+                        try {
+                            assignee_name = "${props.fields.assignee.name}"
+                        }
+                        catch(Exception ex) {
+                            assignee_name = "Default"
+                        }
+                        epic_link = "${props.fields.customfield_12202}"
+                        try{
+                            resolution_name ="${}"
+                        }
+                        catch(Exception ex) {
+                            resolution_name=null
+                        }
+                        attachment_name = "${}"
+                        attachment_content ="${}"
+                        comments="${}"
+                        status_name="${}"
+                        fxver_arch= sh().trim()
+                        fxver_name=sh().trim()
+                        fxver_reldate=sh().trim()
+                        fxver_rel=sh().trim()
+                        labels=sh().trim()
+                        epic_link=sh().trim()
+                        attachment_name=sh().trim()
+                        attachment_content=sh().trim()
+                        comments=sh().trim()
+                        status_name=sh().trim()
+                        
+                        
+                        if (description=="null"){
+                            env="NA"
+                        }
+                        if (env=="null"){
+                            env = "NA"
+                        }
+                        
+                        if (fxver_arch==""){
+                            amap = [
+                                fields:
+                                [
+                                    project:
+                                    [
+                                        key:"FTCPLAT"
+                                    ],
+                                    summary:"${summary_value}",
+                                    issuetype:
+                                    [
+                                        name:"$issuetype_name}"
+                                    ],
+                                    reporter:
+                                    [
+                                        name:"${}"
+                                    ],
+                                    description:"${}"
+                                ],
+                                priority:
+                                [
+                                    name:"${}"
+                                ],
+                                assignee:
+                                [
+                                    name:"${}"
+                                ]
+                            ]
+                        }
+                        
+                        }
+                }
+            }
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}    
+}   
